955137af2c6ef3c51475fb58d144c63b8ee2a4ee
Taited <31286290+Taited@users.noreply.github.com>
2019-12-01 23:05:22 +0800

    可以程序自动拼图的第一个版本
    但是取消了用户点击的功能

--- GamePure.py
+++ GamePure.py
@@ -46,42 +46,43 @@ class GameBoard:
         self.s = self.board_init()
 
     def main(self):
-        s = self.board_init()
+        self.board_init()
         path = self.solver_1()
         counter = 0
         # 游戏主循环
         while True:
             # 延时32毫秒,相当于FPS=30
-            pygame.time.delay(1200)
+            pygame.time.delay(200)
             for event in pygame.event.get():
                 # 窗口的关闭事件
                 if event.type == pygame.QUIT:
                     exit()
-                # elif event.type == pygame.MOUSEBUTTONDOWN:  # 鼠标单击事件
-                #     if pygame.mouse.get_pressed() == (1, 0, 0):  # 鼠标左键按下
-                #         mx, my = pygame.mouse.get_pos()  # 获得当前鼠标坐标
-                #         if mx < 498 and my < 498:  # 判断鼠标是否在操作范围内
-                #             x = int(mx / 166)  # 计算鼠标点到了哪个图块
-                #             y = int(my / 166)
-                #             self.click(x, y)  # 调用单击事件
-                #             if self.map == self.win_map:  # 如果当前地图情况和胜利情况相同,就print胜利
-                #                 print("胜利了！")
+                elif event.type == pygame.MOUSEBUTTONDOWN:  # 鼠标单击事件
+                    if pygame.mouse.get_pressed() == (1, 0, 0):  # 鼠标左键按下
+                        mx, my = pygame.mouse.get_pos()  # 获得当前鼠标坐标
+                        if mx < 498 and my < 498:  # 判断鼠标是否在操作范围内
+                            x = int(mx / 166)  # 计算鼠标点到了哪个图块
+                            y = int(my / 166)
+                            self.click(x, y)  # 调用单击事件
+                            if self.map == self.win_map:  # 如果当前地图情况和胜利情况相同,就print胜利
+                                print("胜利了！")
             # 背景色填充成灰色
             self.s.fill((88, 88, 88))
-            # 绘图
+            if counter + 1 > len(path):
+                time.sleep(60)
+            draw_temp = path[counter]
+            counter = counter + 1
 
-            for y in range(3):
-                for x in range(3):
-                    draw_temp = path[counter]
-                    i = draw_temp[y][x]
-                    counter = counter + 1
-                    if counter+1 >= len(path):
-                        time.sleep(60)
+            # 生成绘图所需要的块
+            for y_id in range(3):
+                for x_id in range(3):
+                    # i = self.map[y][x]
+                    i = draw_temp[y_id][x_id]
                     if i == 0:  # 0号图块不用绘制
                         continue
                     dx = (i % 3) * 166  # 计算绘图偏移量
                     dy = (int(i / 3)) * 166
-                    self.s.blit(self.img, (x * 166, y * 166), (dx, dy, 166, 166))
+                    self.s.blit(self.img, (x_id * 166, y_id * 166), (dx, dy, 166, 166))
             # 画参考图片
             self.s.blit(self.img, (600, 0))
             # 刷新界面
@@ -93,9 +94,15 @@ class GameBoard:
         # 窗口标题
         pygame.display.set_caption('拼图游戏')
         # 窗口大小
-        s = pygame.display.set_mode((1100, 500))
+        s = pygame.display.set_mode((1100, 600))
         # 随机地图
         self.rand_map()
+        # self.map = [
+        #     [7, 5, 1], [4, 8, 3], [2, 6, 0]
+        # ]
+        self.map = [
+            [2, 5, 1], [7, 4, 3], [6, 0, 8]
+        ]
         return s
 
     # 游戏的单击事件
@@ -122,22 +129,10 @@ class GameBoard:
         state = State(temp_init, temp_win)
         path = state.solveProblem(temp_win)
         route = []
-        # TODO path的路径是相反的，记得要倒叙遍历
+        # path的路径是相反的，记得要倒叙遍历
         for mat in reversed(path):
             route.append(convert_blank_to_zero(mat))
         return route
-
-
-def instantiating(init_map, win_map):
-    temp_init = convert_zero_to_blank(copy.deepcopy(init_map))
-    temp_win = convert_zero_to_blank(copy.deepcopy(win_map))
-    state = State(temp_init, temp_win)
-    path = state.solveProblem(temp_win)
-    route = []
-    # TODO path的路径是相反的，记得要倒叙遍历
-    for mat in reversed(path):
-        route.append(convert_blank_to_zero(mat))
-    return route
 
 
 if __name__ == '__main__':
@@ -147,5 +142,4 @@ if __name__ == '__main__':
     # img_PIL = img_PIL.resize((500, 500))
     # img_PIL.save('test.jpeg')
     myGame = GameBoard('./test.jpeg')
-    # route = instantiating(myGame.map, myGame.win_map)
     myGame.main()
